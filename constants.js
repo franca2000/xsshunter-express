a890400555d21ec8d2fa980a364d5791dc47c6ac
franca2000 <73964467+franca2000@users.noreply.github.com>
2025-05-13 16:58:48 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 module.exports = {
-  domain: "xsshunter-express-7f8o.onrender.com", // tu dominio real sin https
+  domain: "xsshunter-express-7f8o.onrender.com", // sin https
   httpPort: process.env.PORT || 3000,
-  useHTTPS: false, // desactivamos HTTPS interno, porque Render ya lo maneja
-  maintainerEmail: "francisco@ejemplo.com", // cualquier email, solo para evitar el error
+  useHTTPS: false,
+  maintainerEmail: "", // vacío, no lo usaremos
   enableLogging: true
 };
+
